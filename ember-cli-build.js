45daf9a7c613b921b968aa656aff274487217609
Peter Janak <2010janak2010@gmail.com>
2016-08-01 19:41:24 +0000

    chore: Installed materialize design - ember-cli-materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,18 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: 'sass',
+      includePaths: [
+        'bower_components/materialize/sass'
+      ],
+      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
