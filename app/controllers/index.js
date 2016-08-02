d4c3ba706b7fc9dca81b8feeafc49d7b524d829b
Peter Janak <2010janak2010@gmail.com>
2016-08-02 03:11:40 +0000

    refactor: Modified autocomplete with ember's get and set

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,21 +3,34 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     initAutocomplete: function(e){
-      console.log(e.currentTarget);
       // Create the autocomplete object, restricting the search to geographical
       // location types.
-      //if(!e.currentTarget.val().length) {return;}
-      this.autocomplete = new google.maps.places.Autocomplete(
+      let autocomplete = new google.maps.places.Autocomplete(
           /** @type {!HTMLInputElement} */(document.getElementById(e.currentTarget.id)),
           {types: ['geocode']});
 
-      this.geolocate(this.autocomplete);
       // When the user selects an address from the dropdown, populate the address
       // fields in the form.
-      this.autocomplete.addListener('place_changed', ()=>{
-        let place = this.autocomplete.getPlace();
+      autocomplete.addListener('place_changed', ()=>{
+        let place = autocomplete.getPlace();
         console.log(place);
       });
+
+      this.geolocate(autocomplete);
+      this.set('autocomplete', autocomplete);
+
+
+
+      // const apiKey = '210cd3d0b88f32603edc631a13ce14f9';
+      // const apiId = '81d2c3ad';
+      // let link = 'http://transportapi.com/v3/uk/public/journey/from/stop:London+bridge/to/stop:Canary+wharf/at/2016-10-18/15:29.json?not_modes=train&api_key='+ apiKey +'&app_id='+ apiId;
+      // console.log(link);
+      // fetch(link).then((response)=>{
+      //   response.json().then((data)=>{
+      //     console.log(data);
+      //   });
+      // });
+
     }
   },
 
