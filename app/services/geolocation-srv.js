e0c5861e7f09c21a9e8d40e89c00c2293526c4f4
Peter Janak <2010janak2010@gmail.com>
2016-08-03 04:30:54 +0100

    refactor: Set autocomplete service
    Moved google's api from home route to service to separate concerns

---
+++ app/services/geolocation-srv.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+
+/**
+* @name Geolocation service
+* @desc Goole`s Autocomplete Api
+* @param string $objectName - name of the object to save return under
+* @param string $elemenId - id of input element
+*/
+export default Ember.Service.extend({
+  routeData: {},
+
+  init: function($elemenId, $objectName){
+      // Create the autocomplete object, restricting the search to geographical
+      // location types.
+      if($elemenId){
+        let autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById($elemenId)),
+            {types: ['geocode']});
+
+        autocomplete.addListener('place_changed', ()=>{
+          let place = autocomplete.getPlace();
+          this.set('routeData.'+ $objectName, place);
+        });
+
+        this.geolocate(autocomplete);
+        this.set('autocomplete', autocomplete);
+      }
+  },
+
+  fetchRouteData: function(){
+    return this.get('routeData');
+  },
+
+  geolocate: function(autocomplete) {
+     // Bias the autocomplete object to the user's geographical location,
+     // as supplied by the browser's 'navigator.geolocation' object.
+     if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function(position) {
+         var geolocation = {
+           lat: position.coords.latitude,
+           lng: position.coords.longitude
+         };
+         var circle = new google.maps.Circle({
+           center: geolocation,
+           radius: position.coords.accuracy
+         });
+         autocomplete.setBounds(circle.getBounds());
+       });
+     }
+   },//geolocate
+
+});
