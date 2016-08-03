c537fd7adb99a5ce1915c240410a21d2202d2325
Peter Janak <2010janak2010@gmail.com>
2016-08-03 04:33:33 +0100

    feat:  Autocomplete service saves data from both inputs,

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  autoComlete: Ember.inject.service('geolocation-srv'),
   actions: {
     fetchData(){
-      console.log('fetching');
-    }
+      console.log(this.get('autoComlete').fetchRouteData());
+    },
+
+    locationFrom(e){
+      // The service require element id and string to name an object
+      this.get('autoComlete').init(e.currentTarget.id, 'from');
+    },
+
+    locationTo(e){
+      this.get('autoComlete').init(e.currentTarget.id, 'to');
+    },
   }
 });
