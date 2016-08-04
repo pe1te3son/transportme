2a48f1d7f7036a7ebfe4e4112db678d66a41029a
Peter Janak <2010janak2010@gmail.com>
2016-08-04 02:38:34 +0100

    feat: Single route component
    Displays and renders each route

---
+++ tests/integration/components/route-single-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('route-single', 'Integration | Component | route single', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{route-single}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#route-single}}
+      template block text
+    {{/route-single}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
