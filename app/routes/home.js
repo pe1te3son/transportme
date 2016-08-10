2d98cca27f80f07e3478f77a6042b2b749472e88
Peter Janak <2010janak2010@gmail.com>
2016-08-10 03:02:19 +0100

    feat: Added json mock data for offline work

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
+  model(){
+    $.get('jsondata.json').then((resp)=>{
+      console.log(resp);
+    });
+    return $.get('jsondata.json');
+  },
 });
