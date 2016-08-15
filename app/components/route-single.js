177a518973f9a2e94a38a887ee883621bb3aed70
Peter Janak <2010janak2010@gmail.com>
2016-08-15 23:28:49 +0000

    refactor: Route details expand on click

--- app/components/route-single.js
+++ app/components/route-single.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+  tagName: 'li',
+  classNames: ['col s12 route-single card'],
+
+  click(e){
+    console.log(e);
+    $(e.currentTarget).find('.route-details').toggleClass('hide');
+  }
 });
--- app/routes/home/route-details.js
+++ app/routes/home/route-details.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     console.log(params);
-    console.log(this.get('store').findRecord('routes', params.departure_time));
+    //console.log(this.get('store').findRecord('routes', params.departure_time));
   }
 });
