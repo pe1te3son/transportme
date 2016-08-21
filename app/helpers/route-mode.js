c2d03132e21c376737c1bdb0a567362fa2ed8fb4
Peter Janak <2010janak2010@gmail.com>
2016-08-21 11:07:10 +0000

    feat: Sets icon for each route mode
    Returns font-awesome class

--- app/helpers/route-mode.js
+++ app/helpers/route-mode.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
 
-export function routeMode(mode/*, hash*/) {
-  return mode;
+export function routeMode(params/*, hash*/) {
+  let [mode] = params;
+  let routeIcon = '';
+
+  switch (mode) {
+    case 'foot':
+      routeIcon = 'male';
+      break;
+
+    case 'tube':
+      routeIcon = 'subway';
+      break;
+
+    case 'bus':
+      routeIcon = 'bus';
+      break;
+
+    case 'train':
+      routeIcon = 'train';
+      break;
+  }
+  return routeIcon;
 }
 
 export default Ember.Helper.helper(routeMode);
