3baef250fd3c138d89a6388f748208625f991454
Peter Janak <2010janak2010@gmail.com>
2016-08-29 02:46:19 +0000

    feat: Displays detailed route instructions based on route mode

---
+++ tests/integration/components/route-instructions-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('route-instructions', 'Integration | Component | route instructions', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{route-instructions}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#route-instructions}}
+      template block text
+    {{/route-instructions}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
