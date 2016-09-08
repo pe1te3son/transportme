b1c719a24dbac1613f0732d959cd726f954925af
Peter Janak <2010janak2010@gmail.com>
2016-09-08 22:35:21 +0000

    chore: Added indexeDB-promised

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -28,6 +28,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import('bower_components/pickadate/lib/picker.time.js');
+  app.import('vendor/indexedDB-promised.js');
 
   return app.toTree();
 };
