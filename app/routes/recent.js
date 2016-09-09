6a0ecee3d3198b46a52ba43c095713ddb525fe18
Peter Janak <2010janak2010@gmail.com>
2016-09-09 01:17:53 +0000

    refactor: Set indexedDb defaults for recent page

--- app/routes/recent.js
+++ app/routes/recent.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   indexedDbPromised: Ember.inject.service('indexed-db'),
+  recentSearchDb: {
+    dbName: 'transportme-v1',
+    store: 'recent'
+  },
   model(){
-    return this.get('indexedDbPromised').getRoutes({dbName: 'transportme-v1', store: 'recent'}).then((resp)=>{
+    return this.get('indexedDbPromised').getRoutes(this.get('recentSearchDb')).then((resp)=>{
       return resp;
     });
   }
