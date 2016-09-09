9254e3f76ba51ab93342239ef196890c4ed27b1e
Peter Janak <2010janak2010@gmail.com>
2016-09-09 01:51:46 +0000

    feat: Set model to request data from indexedDB

--- app/routes/recent.js
+++ app/routes/recent.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  indexedDbPromised: Ember.inject.service('indexed-db'),
+  model(){
+    return this.get('indexedDbPromised').getRoutes({dbName: 'transportme-v1', store: 'recent'}).then((resp)=>{
+      return resp;
+    });
+  }
 });
