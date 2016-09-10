53360146a0491bbcf89618258eb377f5c239e2d7
Peter Janak <2010janak2010@gmail.com>
2016-09-10 01:40:27 +0000

    feat: Returns start or end destination

---
+++ app/helpers/from-to.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export function fromTo(params/*, hash*/) {
+  let [ model, direction] = params;
+
+  if (!Array.prototype.last){
+      Array.prototype.last = function(){
+          return this[this.length - 1];
+      };
+  }
+
+  if(!model || !model.routes || !direction){ return; }
+
+  if(direction === 'from'){
+    return model.routes[0].route_parts[0].from_point_name;
+  } else if(direction === 'to'){
+    return model.routes[0].route_parts.last().to_point_name;
+  }
+
+
+}
+
+export default Ember.Helper.helper(fromTo);
---
+++ tests/unit/helpers/from-to-test.js
@@ -0,0 +1,10 @@
+import { fromTo } from 'transportme/helpers/from-to';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | from to');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = fromTo([42]);
+  assert.ok(result);
+});
