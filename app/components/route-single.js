db862aed67fff8647786787e503f71298ae4ead5
Peter Janak <2010janak2010@gmail.com>
2016-09-13 03:18:03 +0000

    feat: Add to favorite button now saves to indexeddb on click

--- app/components/route-single.js
+++ app/components/route-single.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Component.extend({
+  indexedDbPromised: Ember.inject.service('indexed-db'),
   tagName: 'li',
   classNames: ['col s12 route-single card'],
 
@@ -14,7 +15,21 @@ export default Ember.Component.extend({
     },
 
     addToFavorites(e){
-      console.log(e);
+
+      // TODO move prototype to global, refactor long string
+      if (!Array.prototype.last){
+          Array.prototype.last = function(){
+              return this[this.length - 1];
+          };
+      }
+      this.get('indexedDbPromised').saveToDb({
+        $dbName: 'transportme-favorites',
+        $dbStore: 'favorites',
+        $key: `${JSON.parse(JSON.stringify(e.route_parts[0].from_point_name))}->${JSON.parse(JSON.stringify(e.route_parts.last().to_point_name))}`,
+        $value: JSON.parse(JSON.stringify(e))
+      }).then(()=>{
+        console.log('added to favorites');
+      });
     }
   }
 
