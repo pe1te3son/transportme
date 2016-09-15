9dd57a9b04a34424628254f80b4c9fd57d4c097d
Peter Janak <2010janak2010@gmail.com>
2016-09-15 01:56:48 +0000

    refactor: Removed unnecessary variable

--- app/routes/recent.js
+++ app/routes/recent.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   indexedDbPromised: Ember.inject.service('indexed-db'),
-  dbStore: 'journeys',
   model(){
     return this.get('indexedDbPromised').getRoutes({
       $dbName: 'transportme-recent',
