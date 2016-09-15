3f0955b5fd980ecd9125c0d4d079027a8fe0d5aa
Peter Janak <2010janak2010@gmail.com>
2016-09-15 01:57:48 +0000

    feat: Added observer for inputs. Not hooked up yet

--- app/controllers/recent.js
+++ app/controllers/recent.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  toLocation: null,
+  fromLocation: null,
+  // Watch for changes on input
+  fromLocationObserver: function(){
+
+    //Search based on "from location" input if found update "to location"
+    let routeFound = this.get('model').findBy('from', this.get('fromLocation'));
+    if(!routeFound){
+      return this.set('toLocation', null);
+    }
+    this.set('toLocation', routeFound.to);
+  }.observes('fromLocation'),
+
+  // toLocationObserver: function(){
+  //
+  //   //Search based on "from location" input if found update "to location"
+  //   let routeFound = this.get('model').findBy('to', this.get('toLocation'));
+  //   if(!routeFound){
+  //     return this.set('fromLocation', null);
+  //   }
+  //   this.set('fromLocation', routeFound.from);
+  // }.observes('toLocation'),
+
+  actions: {
+    test(e){
+      console.log(this.buildList('to'));
+    }
+  },
+
 });
