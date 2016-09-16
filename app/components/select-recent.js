29804be518e3d5bc94999f1ca063bd1ae7d7c88a
Peter Janak <2010janak2010@gmail.com>
2016-09-16 02:05:33 +0000

    feat: Added component that return selected value

---
+++ app/components/select-recent.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  actions: {
+    itemselected(item){
+      // return selected value to initiator
+      this.sendAction('optionSelected', item);
+    }
+  },
+
+  didInsertElement: function(){
+    this.$('select').material_select();
+  },
+
+  listChanged: function(){
+
+    // Material desing doesnt change optsion automaticly
+    // I have to rerender them manually if list changes
+    return new Promise((resolve, reject)=>{
+
+      const el = this.$('select');
+      if(!el){
+        reject();
+      }
+      resolve(el.material_select('destroy'));
+
+    }).then(()=> this.$('select').material_select() )
+      .catch(()=> console.error('Element not found!'));
+
+
+  }.observes('list')
+
+});
