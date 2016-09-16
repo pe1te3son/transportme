00990b0355f3ebc9cd5a0aaa1553dd2681ac9baf
Peter Janak <2010janak2010@gmail.com>
2016-09-16 04:06:14 +0000

    chore: Removed console.logs

--- app/helpers/uniq-list.js
+++ app/helpers/uniq-list.js
@@ -3,19 +3,13 @@ import Ember from 'ember';
 export function uniqList(params/*, hash*/) {
 
   const [ $array, $key, $optional ] = params;
-  console.log($optional);
 
   if(!$optional){
-      console.log($array);
     if($array){
       return $array.getEach($key).uniq();
     }
   }
   return $optional.getEach($key).uniq();
-
-
-
-
 
 }
 
