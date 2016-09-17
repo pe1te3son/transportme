8da2abd5adf2836ba1a118ac59d290848117837b
Peter Janak <2010janak2010@gmail.com>
2016-09-17 01:11:23 +0000

    feat: Load data for Favorites page

--- app/routes/favorites.js
+++ app/routes/favorites.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  indexedDbPromised: Ember.inject.service('indexed-db'),
+  model(){
+    return this.get('indexedDbPromised').getRoutes({
+      $dbName: 'transportme-favorites',
+      $dbStore: 'favorites'
+    }).then((resp)=>{
+      return resp;
+    });
+  },
 });
