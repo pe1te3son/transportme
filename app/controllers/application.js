8ea28156fc58cde4cd8fb97a37b73664bae227e7
Peter Janak <2010janak2010@gmail.com>
2016-09-19 04:10:17 +0000

    refactor: Wrapped dabase init in ember run once method

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,19 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   indexedDbPromised: Ember.inject.service('indexed-db'),
   init(){
-    this.get('indexedDbPromised').createIndexedDbStore({
-      $dbName: 'transportme-recent',
-      $dbStore: 'recent',
-      $dbVersion: 1,
-      $keyPath: 'fromTo',
-      $index: ['by-date', 'request_time']
+    Ember.run.once(()=>{
+      this.get('indexedDbPromised').createIndexedDbStore({
+        $dbName: 'transportme-favorites',
+        $dbStore: 'favorites',
+        $dbVersion: 1,
+        $keyPath: 'favId'
+      });
+
+      this.get('indexedDbPromised').createIndexedDbStore({
+        $dbName: 'transportme-recent',
+        $dbStore: 'recent',
+        $dbVersion: 1,
+        $keyPath: 'fromTo',
+        $index: ['by-date', 'request_time']
+      });
+
     });
 
-    this.get('indexedDbPromised').createIndexedDbStore({
-      $dbName: 'transportme-favorites',
-      $dbStore: 'favorites',
-      $dbVersion: 2,
-      $keyPath: 'favId'
-    });
   },
 });
