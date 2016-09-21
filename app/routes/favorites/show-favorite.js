470627f8124ae762e3da0702e8a83c7e844f1149
Peter Janak <2010janak2010@gmail.com>
2016-09-21 00:45:03 +0000

    feat: Fetch favorite journey from indexedDb by passed url

--- app/routes/favorites/show-favorite.js
+++ app/routes/favorites/show-favorite.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  indexedDbPromised: Ember.inject.service('indexed-db'),
   model(params){
-    return params;
+
+    return this.get('indexedDbPromised').getById({
+      $dbName: 'transportme-favorites',
+      $dbStore: 'favorites',
+      $id: params.fromTo
+    });
+    
   }
 });
