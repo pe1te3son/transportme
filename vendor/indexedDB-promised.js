a16d4dccbf244ab70cc026a31e42969c491fc05d
Peter Janak <2010janak2010@gmail.com>
2016-10-19 04:17:46 +0100

    style: Replace single with double quotes

--- vendor/indexedDB-promised.js
+++ vendor/indexedDB-promised.js
@@ -38,7 +38,7 @@
   function proxyProperties(ProxyClass, targetProp, properties) {
     properties.forEach(function(prop) {
       Object.defineProperty(ProxyClass.prototype, prop, {
-        get: function() {
+        get() {
           return this[targetProp][prop];
         }
       });
@@ -47,7 +47,7 @@
 
   function proxyRequestMethods(ProxyClass, targetProp, Constructor, properties) {
     properties.forEach(function(prop) {
-      if (!(prop in Constructor.prototype)) return;
+      if (!(prop in Constructor.prototype)) { return; }
       ProxyClass.prototype[prop] = function() {
         return promisifyRequestCall(this[targetProp], prop, arguments);
       };
@@ -76,24 +76,24 @@
     this._index = index;
   }
 
-  proxyProperties(Index, '_index', [
-    'name',
-    'keyPath',
-    'multiEntry',
-    'unique'
+  proxyProperties(Index, "_index", [
+    "name",
+    "keyPath",
+    "multiEntry",
+    "unique"
   ]);
 
-  proxyRequestMethods(Index, '_index', IDBIndex, [
-    'get',
-    'getKey',
-    'getAll',
-    'getAllKeys',
-    'count'
+  proxyRequestMethods(Index, "_index", IDBIndex, [
+    "get",
+    "getKey",
+    "getAll",
+    "getAllKeys",
+    "count"
   ]);
 
-  proxyCursorRequestMethods(Index, '_index', IDBIndex, [
-    'openCursor',
-    'openKeyCursor'
+  proxyCursorRequestMethods(Index, "_index", IDBIndex, [
+    "openCursor",
+    "openKeyCursor"
   ]);
 
   function Cursor(cursor, request) {
@@ -101,20 +101,20 @@
     this._request = request;
   }
 
-  proxyProperties(Cursor, '_cursor', [
-    'direction',
-    'key',
-    'primaryKey',
-    'value'
+  proxyProperties(Cursor, "_cursor", [
+    "direction",
+    "key",
+    "primaryKey",
+    "value"
   ]);
 
-  proxyRequestMethods(Cursor, '_cursor', IDBCursor, [
-    'update',
-    'delete'
+  proxyRequestMethods(Cursor, "_cursor", IDBCursor, [
+    "update",
+    "delete"
   ]);
 
-  // proxy 'next' methods
-  ['advance', 'continue', 'continuePrimaryKey'].forEach(function(methodName) {
+  // proxy "next" methods
+  ["advance", "continue", "continuePrimaryKey"].forEach(function(methodName) {
     if (!(methodName in IDBCursor.prototype)) return;
     Cursor.prototype[methodName] = function() {
       var cursor = this;
@@ -141,31 +141,31 @@
     return new Index(this._store.index.apply(this._store, arguments));
   };
 
-  proxyProperties(ObjectStore, '_store', [
-    'name',
-    'keyPath',
-    'indexNames',
-    'autoIncrement'
+  proxyProperties(ObjectStore, "_store", [
+    "name",
+    "keyPath",
+    "indexNames",
+    "autoIncrement"
   ]);
 
-  proxyRequestMethods(ObjectStore, '_store', IDBObjectStore, [
-    'put',
-    'add',
-    'delete',
-    'clear',
-    'get',
-    'getAll',
-    'getAllKeys',
-    'count'
+  proxyRequestMethods(ObjectStore, "_store", IDBObjectStore, [
+    "put",
+    "add",
+    "delete",
+    "clear",
+    "get",
+    "getAll",
+    "getAllKeys",
+    "count"
   ]);
 
-  proxyCursorRequestMethods(ObjectStore, '_store', IDBObjectStore, [
-    'openCursor',
-    'openKeyCursor'
+  proxyCursorRequestMethods(ObjectStore, "_store", IDBObjectStore, [
+    "openCursor",
+    "openKeyCursor"
   ]);
 
-  proxyMethods(ObjectStore, '_store', IDBObjectStore, [
-    'deleteIndex'
+  proxyMethods(ObjectStore, "_store", IDBObjectStore, [
+    "deleteIndex"
   ]);
 
   function Transaction(idbTransaction) {
@@ -184,13 +184,13 @@
     return new ObjectStore(this._tx.objectStore.apply(this._tx, arguments));
   };
 
-  proxyProperties(Transaction, '_tx', [
-    'objectStoreNames',
-    'mode'
+  proxyProperties(Transaction, "_tx", [
+    "objectStoreNames",
+    "mode"
   ]);
 
-  proxyMethods(Transaction, '_tx', IDBTransaction, [
-    'abort'
+  proxyMethods(Transaction, "_tx", IDBTransaction, [
+    "abort"
   ]);
 
   function UpgradeDB(db, oldVersion, transaction) {
@@ -203,15 +203,15 @@
     return new ObjectStore(this._db.createObjectStore.apply(this._db, arguments));
   };
 
-  proxyProperties(UpgradeDB, '_db', [
-    'name',
-    'version',
-    'objectStoreNames'
+  proxyProperties(UpgradeDB, "_db", [
+    "name",
+    "version",
+    "objectStoreNames"
   ]);
 
-  proxyMethods(UpgradeDB, '_db', IDBDatabase, [
-    'deleteObjectStore',
-    'close'
+  proxyMethods(UpgradeDB, "_db", IDBDatabase, [
+    "deleteObjectStore",
+    "close"
   ]);
 
   function DB(db) {
@@ -222,21 +222,21 @@
     return new Transaction(this._db.transaction.apply(this._db, arguments));
   };
 
-  proxyProperties(DB, '_db', [
-    'name',
-    'version',
-    'objectStoreNames'
+  proxyProperties(DB, "_db", [
+    "name",
+    "version",
+    "objectStoreNames"
   ]);
 
-  proxyMethods(DB, '_db', IDBDatabase, [
-    'close'
+  proxyMethods(DB, "_db", IDBDatabase, [
+    "close"
   ]);
 
   // Add cursor iterators
   // TODO: remove this once browsers do the right thing with promises
-  ['openCursor', 'openKeyCursor'].forEach(function(funcName) {
+  ["openCursor", "openKeyCursor"].forEach(function(funcName) {
     [ObjectStore, Index].forEach(function(Constructor) {
-      Constructor.prototype[funcName.replace('open', 'iterate')] = function() {
+      Constructor.prototype[funcName.replace("open", "iterate")] = function() {
         var args = toArray(arguments);
         var callback = args[args.length - 1];
         var request = (this._store || this._index)[funcName].apply(this._store, args.slice(0, -1));
@@ -274,7 +274,7 @@
 
   var exp = {
     open: function(name, version, upgradeCallback) {
-      var p = promisifyRequestCall(indexedDB, 'open', [name, version]);
+      var p = promisifyRequestCall(indexedDB, "open", [name, version]);
       var request = p.request;
 
       request.onupgradeneeded = function(event) {
@@ -288,11 +288,11 @@
       });
     },
     delete: function(name) {
-      return promisifyRequestCall(indexedDB, 'deleteDatabase', [name]);
+      return promisifyRequestCall(indexedDB, "deleteDatabase", [name]);
     }
   };
 
-  if (typeof module !== 'undefined') {
+  if (typeof module !== "undefined") {
     module.exports = exp;
   }
   else {
