fed314d9f9aed9e5bebb0f25ae45f43768976576
Peter Janak <2010janak2010@gmail.com>
2016-11-11 03:50:26 +0000

    feat: Fetch initial station based on current user location

--- app/routes/trains.js
+++ app/routes/trains.js
@@ -1,15 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  geolocateSrv: Ember.inject.service('geolocation-srv'),
+
   model () {
-    const url = 'http://transportapi.com/v3/uk/train/station/VIC/2016-11-22/10:00/timetable.json?app_id=81d2c3ad&app_key=210cd3d0b88f32603edc631a13ce14f9&train_status=passenger';
-    return fetch(url)
+    return this.get('geolocateSrv').getCurrentCoordinates()
+      .then(location => {
+        return this.getNearbyStations(location);
+      })
+      .then(response => {
+        return response;
+      });
+  },
+
+  getNearbyStations (location) {
+    return fetch(`http://transportapi.com/v3/uk/train/stations/near.json?app_id=81d2c3ad&app_key=210cd3d0b88f32603edc631a13ce14f9&lat=${location.lat}&lon=${location.lng}`)
       .then(response => {
         return response.json();
-      })
-      .then(jsonResponse => {
-        console.log(jsonResponse);
-        return jsonResponse;
       });
+  },
+
+  setupController: function (controller, model) {
+    this._super(controller, model);
+    this.controllerFor('trains').set('timetable', null);
+    Ember.run.schedule('afterRender', () => {
+      this.controllerFor('trains').setInitialState();
+    });
   }
 });
